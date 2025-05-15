5b5ad045906b37392ab346f670d18bd3d45e1f1d
Mingtindu <mingmartu@gmail.com>
2025-05-15 10:41:44 +0545

    first commit

---
+++ defaultModules.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+import os from "os";
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.userInfo());
+console.log(os.uptime());
+
+// fs.writeFileSync("hello.txt","this is file writng ");
---
+++ first.js
@@ -0,0 +1,6 @@
+import {add} from "./add.js"
+const sum = add(2,3);
+// const s = sub(2,3);
+
+console.log(sum);
+// console.log(s);
